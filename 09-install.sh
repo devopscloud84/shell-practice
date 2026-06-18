13e535f4cdff4adae86b05e913846c4a33f16972
joindevopscloud <info@devoos.com>
2026-06-18 20:38:34 +0530

    hellow world

---
+++ 09-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+# check the root access or not
+
+if [ $USERID -ne 0 ]; then 
+    echo "Please run the script with root access"
+    exit 1
+fi
+echo "Im continuing..."
