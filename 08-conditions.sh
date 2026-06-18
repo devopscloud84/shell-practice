28f67fc8c4932449993d508877a3a6309bd2be61
joindevopscloud <info@devoos.com>
2026-06-18 18:17:56 +0530

    hellow world

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,9 +3,9 @@
 NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
-echo "Given Number $NUMBER is greater than to 20"
-elif [ $NUMBER -ge 20 ]
-echo  "Given Number $NUMBER is equal to 20"
+    echo "Given Number $NUMBER is greater than to 20"
+elif [ $NUMBER -eq 20 ]
+    echo  "Given Number $NUMBER is equal to 20"
 else
-echo  "Given Number $NUMBER is lessthan to 20"
+    echo  "Given Number $NUMBER is lessthan to 20"
 fi
