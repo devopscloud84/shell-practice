5c0118e15956b9c4792eac6046defadd8f5e672e
joindevopscloud <info@devoos.com>
2026-06-19 21:01:55 +0530

    hellow world

---
+++ 10.logs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=home/ec2-user/shell-logs
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+# check the root access or not
+
+if [ $USERID -ne 0 ]; then 
+    echo "Please run the script with root access"
+    exit 1
+fi
+
+# first arg --> what are you trying to install
+# second arg--> exit code
+
+VALIDATE ()
+{
+     if [ $2 -ne 0 ]; then
+    echo "Installing $1 is ...Failed"
+    exit 1
+    else
+    echo "Installing $1 is ...Success"
+    fi
+    
+}
+
+dnf list installed MySQL &>> $LOGS_FILE
+
+if [ $? -eq 0 ]; then
+    echo "MySQL is already installed...Skipping"
+else
+    echo "Insatlling MySQL"
+    dnf install mysql -y &>> $LOGS_FILE
+    VALIDATE MySQL $?
+fi 
+
+dnf list installed nginx &>> $LOGS_FILE
+if [ $? -eq 0 ]; then
+    echo "nginx is already installed... skipping"
+else
+    echo "installing nginx" 
+    dnf install nginx -y &>> $LOGS_FILE
+    VALIDATE nginx $?
+fi
