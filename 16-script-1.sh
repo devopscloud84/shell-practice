d69f6a54ff004427d26bb2f352339e7200f457c6
joindevopscloud <info@devoos.com>
2026-07-22 19:47:17 +0530

    api doc added

---
+++ 16-script-1.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+COUNTRY = INDIA
+echo "country: $COUNTRY"
+echo "PID of script-1: $$"
+
+sh 17-script-2.sh
+
---
+++ 17-script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Iam in :$COUNTRY"
+echo "PID of script-2: $$"
